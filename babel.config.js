5758e1866eb9324b4618bdfbf729403f4323a911
Vittorianno <132303321+Vittorianno@users.noreply.github.com>
2026-09-06 11:38:23 +0100

    fix: adiciona plugins Babel para suportar propriedades privadas (#campo)
    Corrige "SyntaxError: private properties are not supported" causado
    por uma dependência que usa sintaxe de campos/métodos privados de
    classe (ES2022) não coberta pelo babel-preset-expo por padrão.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   let plugins = [];
 
+  plugins.push("@babel/plugin-transform-private-methods");
+  plugins.push("@babel/plugin-transform-class-properties");
+  plugins.push("@babel/plugin-transform-private-property-in-object");
+
   plugins.push("react-native-worklets/plugin");
 
   return {
